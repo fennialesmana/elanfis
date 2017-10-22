06b1903ad1533d81aab3de014990f839afec5195
Tri Fennia Lesmana <tfennia@gmail.com>
2017-10-22 21:43:05 +0630

    delete a file

--- getColumnCode.m
+++
@@ -1,9 +0,0 @@
-function [E]=getColumnCode(num)
-    angka = num;
-    E = '';
-    while angka > 0
-        sisa = mod((angka - 1), 26);
-        E = strcat(char(65 + sisa),E);
-        angka = floor((angka - sisa)/26);
-    end
-end
